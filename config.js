56ca880f6b5e48eef7a3ef4ee30d3fd61df092ed
Paha3555 <sbot08016@gmail.com>
2024-09-27 19:55:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "sH11VRCJ#MSN1BGh5b2cG_o59Gp2z0PFB2UUbaoL6p-m5kVtQy1E",
+MONGODB: process.env.MONGODB || "mongodb://mongo:tZqOskNPFBvoJwXqFTbicqRdmVMzqKtt@autorack.proxy.rlwy.net:59140",//enter mongo db url
 };
