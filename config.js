9befce8eefc53fd344bcbd637d1b3f25a02fbb7a
Paha3555 <sbot08016@gmail.com>
2024-09-28 20:16:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "sH11VRCJ#MSN1BGh5b2cG_o59Gp2z0PFB2UUbaoL6p-m5kVtQy1E",
+SESSION_ID: process.env.SESSION_ID || "zf5QibQS#v_g4DwYmBkxeO3prNdFbJzjvSVCG0hw2m1ZVwVWZQPs",
 MONGODB: process.env.MONGODB || "mongodb://mongo:tZqOskNPFBvoJwXqFTbicqRdmVMzqKtt@autorack.proxy.rlwy.net:59140",//enter mongo db url
 };
